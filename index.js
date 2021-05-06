5b6899ac1ebb27233e63a8c1f9deb64ef61c00fc
Dylan Chance <dylan.chance@energytoolbase.com>
2021-05-06 17:16:05 -0400

    interacting with a slack channel

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
 const { createEventAdapter } = require("@slack/events-api");
+const { startsWith } = require("lodash");
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const slackEvents = createEventAdapter(slackSigningSecret);
 const port = process.env.PORT || 3000;
 
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
 slackEvents.on("message", (event) => {
-  console.log(
-    `Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`
-  );
+  console.log(event);
+
+  if (isCommand(event.text)) {
+    console.log("this is a command");
+    console.log(parseCommand(event.text));
+  } else {
+    console.log("this is not a command");
+  }
 });
 
 (async () => {
@@ -18,3 +24,18 @@ slackEvents.on("message", (event) => {
   // Log a message when the server is ready
   console.log(`Listening for events on ${server.address().port}`);
 })();
+
+function isCommand(text) {
+  const normalizedText = text.toLowerCase();
+
+  return (
+    normalizedText.startsWith("$buy") || normalizedText.startsWith("$sell")
+  );
+}
+
+function parseCommand(text) {
+  text = text.replace("$", "");
+  const parts = text.split(" ");
+
+  return { side: parts[0], qty: parts[1], symbol: parts[2] };
+}
