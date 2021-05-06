1eba41ce5b76e4af6bffe2c5259e67b174248d57
Dylan Chance <dylan.chance@energytoolbase.com>
2021-05-06 16:31:16 -0400

    wip

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { createEventAdapter } = require("@slack/events-api");
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const slackEvents = createEventAdapter(slackSigningSecret);
+const port = process.env.PORT || 3000;
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on("message", (event) => {
+  console.log(
+    `Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`
+  );
+});
+
+(async () => {
+  // Start the built-in server
+  const server = await slackEvents.start(port);
+
+  // Log a message when the server is ready
+  console.log(`Listening for events on ${server.address().port}`);
+})();
